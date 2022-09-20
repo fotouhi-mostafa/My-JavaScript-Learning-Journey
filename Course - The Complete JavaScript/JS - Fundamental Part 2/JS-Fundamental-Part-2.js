4aba95dce74df82ff60486f4a260127e1566992d
Mostafa <mfotouhi8@gmail.com>
2022-09-20 20:54:42 +0430

    docs: update fundamental part 2 readme file & doing fandamental part 2 challange

--- Course - The Complete JavaScript/JS - Fundamental Part 2/JS-Fundamental-Part-2.js
+++ Course - The Complete JavaScript/JS - Fundamental Part 2/JS-Fundamental-Part-2.js
@@ -1,1 +1,78 @@
+"use strict"
 ///////////////////////////////////////////// Coding Challange #1 /////////////////////////////////////////////
+
+// const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+// const calcAverageDolphins = calcAverage(44,23,71);
+// const calcAverageKoalas = calcAverage(65,54,49);
+
+// const checkWinner = function(avgDolphins, avgKoalas) {
+//   if(avgDolphins >= 2 * avgKoalas) {
+//     return console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+//   } else if (avgKoalas >= 2 * avgDolphins) {
+//     return console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+//   } else {
+//     return  console.log(`No teams win`);
+//   }
+// }
+
+// checkWinner(calcAverageDolphins,calcAverageKoalas);
+
+///////////////////////////////////////////// Coding Challange #2 /////////////////////////////////////////////
+
+// const calcTip = (bill) => bill > 50 && bill < 300 ? bill * 0.15 : bill * 0.2;
+
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const totals = [bills[0] + tips[0],bills[1] + tips[1],bills[2] + tips[2]];
+
+// console.log(bills, tips, totals);
+
+///////////////////////////////////////////// Coding Challange #3 /////////////////////////////////////////////
+
+// const mark = {
+//   fullName: 'Mark Miller',
+//   mass: 75,
+//   height: 1.69,
+//   calcBMI: function() {
+//     return this.mass / (this.height * this.height)
+//   }
+// }
+// const john = {
+//   fullName: 'John Smith',
+//   mass: 92,
+//   height: 1.95,
+//   calcBMI: function() {
+//     return this.mass / (this.height * this.height)
+//   }
+// }
+
+// const higherBMI = mark.calcBMI() > john.calcBMI() ?
+  // console.log(`Mark's BMI (${mark.calcBMI()}) is higher than John's BMI (${john.calcBMI()})`)
+  // : console.log(`John's BMI (${john.calcBMI()}) is higher than Mark's BMI (${mark.calcBMI()})`);
+
+///////////////////////////////////////////// Coding Challange #3 /////////////////////////////////////////////
+
+// const bills = [22,295,176,440,37,105,10,1100,86,52];
+// const tips = [];
+// const totals = [];
+// const calcTip = (bill) => bill > 50 && bill < 300 ? bill * 0.15 : bill * 0.2;
+
+// for(let bill = 0; bill < bills.length; bill++) {
+//   const tip = calcTip(bills[bill]);
+//   tips.push(tip);
+//   totals.push(bills[bill] + tip);
+// }
+
+// console.log(tips, totals);
+
+// const calcAverage = function(arr) {
+//   let sum = 0;
+//   for(let item = 0; item < arr.length; item++) {
+//     sum += arr[item];
+//   }
+//   const avg = sum / arr.length;
+//   return sum, avg;
+// }
+
+// console.log(calcAverage(bills));
--- Course - The Complete JavaScript/JS - Fundamental Part 2/JS-Fundamental-Part-2.md
+++ Course - The Complete JavaScript/JS - Fundamental Part 2/JS-Fundamental-Part-2.md
@@ -3,3 +3,17 @@
 **📝 Tip1: We can use 'strict mode' for blocks and functions but it's better to use it at the beginning of the JS file (line 1)**
 
 **📝 Tip2: The big difference between 'function declaration' and 'function expression' is we can call 'function declaration' before defined in the code**
+
+## Useful Array methods
+- push() : The push() method adds one or more elements to the end of an array and returns the new length of the array.
+- unshift() : The unshift() method adds one or more elements to the beginning of an array and returns the new length of the array.
+- pop() : The pop() method removes the last element from an array and returns that element. 
+- shift() : The shift() method removes the first element from an array and returns that removed element.
+- indexOf() : The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
+- includes() : The includes() method determines whether an array includes a certain value among its entries, returning true or false as appropriate.
+
+**📝 Tip3: The big difference between 'Arrays' and 'Objects' is that ordering is important in the arrays but in the objects is not.**
+
+**📝 Tip4: We can create another property for Object in the method of Object with "this" keyword.**
+
+**📝 Tip5: We use "for loop" when we know how many times the loop wants to repeat, so if the counter didn't be importance we use the "while loop".**
